b0a1605c3f0deb8076388506302eccb7f2023d22
peterli98 <pedurli98@gmail.com>
2017-01-15 23:03:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def search_spider():
+def search_spider(max_page):
+    page = 0
+    while page <= max_page:
+        url = "https://myanimelist.net/topanime.php?limit=" + str(page) //save desired url to a variable
+        source_code = requests.get(url) #get the information of that link
+        plain_text = source_code.text #strip the source code of that link store it in variable plain_text
+        soup = BeautifulSoup(plain_text, 'html.parser') #turn the source code into an object so we can apply methods
+        for link in soup.findAll('a' ,{'class': 'hoverinfo_trigger fl-l fs14 fw-b'}): #this specific point in this html
+            title = link.string #get the title
+            href = link.get('href') #the the url of this link 
+            print (title) 
+            print(href)
+            single_item(href) #calls function
+        page += 50
 
-        url = "https://www.google.ca/?gfe_rd=cr&ei=oSMtWJj1OKSC8QettpjQAw#q=swimming&start=" + str(start)
-        source_code = requests.get(url) #get all the source codes and store them in a variable
-        plain_text = source_code.text #get all the texts from source codes and store them in a variable
-        soup = BeautifulSoup(plain_text,"html.parser") #transform the data from plain_text to a format beautifulsoup can read and store in soup
-        for link in soup.findAll("a" ,{"onmousedown":"return rwt"}):
-            title = link.string
-            print(title)
-        start += 10
 
 
-search_spider(0)
+def single_item (item_url):
+    source_code = requests.get(item_url) 
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'html.parser')  
+
+    for score in soup.findAll('span', {'itemprop': 'ratingValue'}): #get rating
+        print("Score: " + score.string)
+
+search_spider(50)
